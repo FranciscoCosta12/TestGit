ac6ba8f201abb77d457999abc20ced6c9c14bec3
Francisco Costa <frcosta@ipvc.com>
2021-03-14 19:11:39 +0000

    Porta Contentores

---
+++ src/git/PortaContentores.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author Francisco Costa
+ */
+public class PortaContentores extends Navio{
+
+    private int contentores;
+    
+    public PortaContentores(String matricula) {
+        super(matricula);
+    }
+
+    /**
+     * @return the contentores
+     */
+    public int getContentores() {
+        return contentores;
+    }
+
+    /**
+     * @param contentores the contentores to set
+     */
+    public void setContentores(int contentores) {
+        this.contentores = contentores;
+    }
+    
+}
