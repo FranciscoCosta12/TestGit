a483bc309652a35c567fd74aeea30b1776b75651
Francisco Costa <frcosta@ipvc.com>
2021-03-14 19:47:59 +0000

    Main

--- src/git/Git.java
+++ src/git/Git.java
@@ -16,6 +16,21 @@ public class Git {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Porto p = new Porto();
+        Navio n = new Navio("1");
+        Petroleiro pt = new Petroleiro("2");
+        PortaContentores pc = new PortaContentores("3");
+        n.setNome("N1");
+        pt.setNome("Pt1");pt.setCapacidade(450);
+        pc.setNome("pc1");pc.setContentores(100);
+        p.addNavio(n);
+        p.addNavio(pt);
+        p.addNavio(pc);
+        p.addNavio(n);
+        
+        System.out.println(pc.getContentores());
+        
+        System.out.println(pt.getCapacidade());
         
     }
     
