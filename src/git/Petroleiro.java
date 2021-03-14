e1b3e8d372b86d48962943858d95c46bdb7b1616
Francisco Costa <frcosta@ipvc.com>
2021-03-14 19:11:19 +0000

    petroleiro

---
+++ src/git/Petroleiro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author Francisco Costa
+ */
+public class Petroleiro extends Navio{
+
+    private float capacidade;
+    
+    public Petroleiro(String matricula) {
+        super(matricula);
+        
+    }
+
+
+    public float getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(float capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+    
+}
