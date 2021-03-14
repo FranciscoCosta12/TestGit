86b1e1334554f5a2aa3172ca5c40bf7e228659af
Francisco Costa <frcosta@ipvc.com>
2021-03-14 19:13:14 +0000

    Navio

--- src/git/Navio.java
+++ src/git/Navio.java
@@ -10,5 +10,46 @@ package git;
  * @author Francisco Costa
  */
 public class Navio {
+    private String matricula;
+    private String nome;
+    private float comprimento;
+    
+    public Navio(String matricula){
+        this.matricula = matricula;
+    }
+    
+    public String getMatricula(){
+        return matricula;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the comprimento
+     */
+    public float getComprimento() {
+        return comprimento;
+    }
+
+    /**
+     * @param comprimento the comprimento to set
+     */
+    public void setComprimento(float comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    
     
 }
